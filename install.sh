01faecd6625e666daeefc1e0737d17b0ddb87c47
Alpha1337k <oscar@kruithof.io>
2025-01-06 03:17:47 +0100

    Feat: initial state

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# make sure we have pulled in and updated any submodules
+git submodule init
+git submodule update
+
+# folders that should, or only need to be installed for a local user
+useronly=(
+    git
+	zsh
+)
+
+# run the stow command for the passed in directory ($2) in location $1
+stowit() {
+    usr=$1
+    app=$2
+    # -v verbose
+    # -R recursive
+    # -t target
+    stow -v -R -t ${usr} ${app}
+}
+
+echo ""
+echo "Stowing apps for user: ${whoami}"
+
+# install only user space folders
+for app in ${useronly[@]}; do
+    if [[! "$(whoami)" = *"root"*]]; then
+        stowit "${HOME}" $app 
+    fi
+done
+
+echo ""
+echo "##### ALL DONE"
