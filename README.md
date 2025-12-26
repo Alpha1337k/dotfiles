d54406253357951613da313ca2b39986c9630a46
Alpha1337k <oscar@kruithof.io>
2025-12-26 15:21:15 +0100

    Feat: added precommit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dotfiles
+
+Fish: version required > 4
--- dev/format.sh
+++ dev/format.sh
@@ -1,24 +1,65 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "HEHEHREH"
+
 cd "$HOME/dotfiles"
 
-files=(
-	$(find . -type f \( -name "*.sh" -o -name "*.zsh" -o -name ".zshrc" \))
+SOFT=false
+
+if [[ -n "${1:-}" && "$1" == "--soft" ]]; then
+	echo "Running in soft mode. No formatting will be applied."
+	SOFT=true
+fi
+
+sh_files=(
+	$(find . -type f \( -name "*.sh" \))
 	$(find ./commands/bin -type f)
 )
 
-for file in "${files[@]}"; do
+fish_files=(
+	$(find . -type f \( -name "*.fish" \))
+)
+
+has_error=false
+
+function run_format {
+	echo "Running '$1'..."
+
+	set +e
+	result=$($1)
+	exit_code=$?
+	set -e
+
+	if [[ $exit_code -ne 0 ]]; then
+		echo "Error formatting '$1': $result"
+		has_error=true
+	fi
+}
+
+for file in "${sh_files[@]}"; do
 	if [[ -f "$file" ]]; then
-		echo "Formatting $file..."
-
-		set +e
-		result=$(shfmt -w "$file")
-		set -e
-
-		if [[ $? -ne 0 ]]; then
-			echo "Error formatting $file: $result"
+		if $SOFT; then
+			run_format "shfmt -i 4 -d $file 2>/dev/null"
+		else
+			run_format "shfmt -i 4 -w $file"
 		fi
 	fi
 done
+
+for file in "${fish_files[@]}"; do
+	if [[ -f "$file" ]]; then
+		if $SOFT; then
+			run_format "fish_indent -c $file"
+		else
+			run_format "fish_indent -w $file"
+		fi
+	fi
+done
+
+if $has_error; then
+	echo "Some files failed to format."
+	exit 1
+fi
 echo "All files formatted successfully!"
+exit 0
